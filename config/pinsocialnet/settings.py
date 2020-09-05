6282695b5b8239053946dac646c85cf9e80e185d
андрей <klimov.aka@yandex.ru>
2020-09-05 16:35:18 +0300

    add database

--- config/pinsocialnet/settings.py
+++ config/pinsocialnet/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mercower',
+        'USER': 'madipo',
+        'PASSWORD': 'Garuno2611',
+        'HOST': '194.67.113.101',
+        'PORT': '5432',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 

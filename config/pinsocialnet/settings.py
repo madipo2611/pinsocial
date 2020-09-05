61d49425d23ca145fe6b3ffc6510d7a13616ecb9
андрей <klimov.aka@yandex.ru>
2020-09-05 15:35:02 +0300

    version 1.0

--- config/pinsocialnet/settings.py
+++ config/pinsocialnet/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pinsocialnet.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pinsocialnet.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database

7707659d9f9e9f867f5bd89d8c3c33afc6650aea
андрей <klimov.aka@yandex.ru>
2020-09-06 10:31:12 +0300

    add settings

--- pinsocialnet/config/settings.py
+++ pinsocialnet/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'src.profiles',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+       'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   #'AUTH_HEADER_TYPES': ('JWT',),
+}

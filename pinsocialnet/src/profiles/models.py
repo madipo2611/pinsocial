c3d25e473899520d1ac55944fc204ae142ea68af
андрей <klimov.aka@yandex.ru>
2020-09-07 08:16:43 +0300

    add AbstractUser

--- pinsocialnet/src/profiles/models.py
+++ pinsocialnet/src/profiles/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class UserNet(AbstractUser):
     """Castome model user"""
+    middle_name = models.CharField(max_length=50)
+    first_login = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=14)
+    avatar = models.ImageField(upload_to='user/avatar/', blank=True, null=True)
